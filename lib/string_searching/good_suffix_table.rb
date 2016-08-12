7e82d84092eab925e1a515138cfadc06194b6a99
cwrw <abeer.salameh@gmail.com>
2016-08-12 18:54:58 +0300

    Add prefix shift to good suffix table
    We now tackle case 2 of the good suffix table rules: part of the
    matching suffix of the pattern occurs at the beginning of the pattern.
    Using the calculation of the border hash, we iterate through the pattern
    from left to right, and try and find a match for the widest border
    starting at index 0, when we pass that and the border becomes longer
    than the string we should switch to the next widest border

--- lib/string_searching/good_suffix_table.rb
+++ lib/string_searching/good_suffix_table.rb
@@ -1,8 +1,8 @@
 module StringSearching
   class GoodSuffixTable
     attr_reader :pattern, :length, :border, :shift
-    def self.generate_table_one(pattern)
-      new(pattern).generate_table_one
+    def self.generate(pattern)
+      new(pattern).generate
     end
 
     def initialize(pattern)
@@ -11,6 +11,14 @@ module StringSearching
       @border = {}
       @shift = Hash.new(0)
     end
+
+    def generate
+      generate_table_one
+      generate_table_two
+      shift
+    end
+
+    private
 
     def generate_table_one
       index = length
@@ -22,10 +30,7 @@ module StringSearching
         shift[index] = 0
         border[index] = border_occurence
       end
-      shift
     end
-
-    private
 
     def reset_border_if_mismatch!(border_occurence, index) # rubocop:disable Metrics/AbcSize
       while border_occurence <= length && pattern[index - 1] != pattern[border_occurence - 1]
@@ -34,6 +39,14 @@ module StringSearching
       end
 
       border_occurence
+    end
+
+    def generate_table_two
+      border_occurence = border[0]
+      (0..length).each do |index|
+        shift[index] = border_occurence if shift[index].zero?
+        border_occurence = border[border_occurence] if border_occurence == index
+      end
     end
   end
 end
--- spec/string_searching/good_suffix_table_spec.rb
+++ spec/string_searching/good_suffix_table_spec.rb
@@ -1,23 +1,21 @@
 require 'spec_helper'
 
 RSpec.describe StringSearching::GoodSuffixTable do
-  context 'shift table for reoccurance of suffix in pattern' do
-    it "generates shift for PICNIC" do
-      pattern = "PICNIC"
-      subject = described_class.generate_table_one(pattern)
-      expect(subject).to eq(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 3, 5 => 0, 6 => 1)
-    end
+  it "generates shift for PICNIC" do
+    pattern = "PICNIC"
+    subject = described_class.generate(pattern)
+    expect(subject).to eq(0 => 6, 1 => 6, 2 => 6, 3 => 6, 4 => 3, 5 => 6, 6 => 1)
+  end
 
-    it "generates shift table for WORD" do
-      pattern = "WORD"
-      subject = described_class.generate_table_one(pattern)
-      expect(subject).to eq(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 1)
-    end
+  it "generates shift table for WORD" do
+    pattern = "WORD"
+    subject = described_class.generate(pattern)
+    expect(subject).to eq(0 => 4, 1 => 4, 2 => 4, 3 => 4, 4 => 1)
+  end
 
-    it "generates shift for ABBABAB" do
-      pattern = "ABBABAB"
-      subject = described_class.generate_table_one(pattern)
-      expect(subject).to eq(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 2, 5 => 0, 6 => 4, 7 => 1)
-    end
+  it "generates shift for ABBABAB" do
+    pattern = "ABBABAB"
+    subject = described_class.generate(pattern)
+    expect(subject).to eq(0 => 5, 1 => 5, 2 => 5, 3 => 5, 4 => 2, 5 => 5, 6 => 4, 7 => 1)
   end
 end
