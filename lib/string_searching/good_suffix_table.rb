c85d7d6065fabc01dc0c1152c7ef172993f136e8
cwrw <abeer.salameh@gmail.com>
2016-08-12 14:31:56 +0300

    Refactor first good suffix table

--- lib/string_searching/good_suffix_table.rb
+++ lib/string_searching/good_suffix_table.rb
@@ -1,8 +1,8 @@
 module StringSearching
   class GoodSuffixTable
     attr_reader :pattern, :length, :border, :shift
-    def self.generate(pattern)
-      new(pattern).generate
+    def self.generate_table_one(pattern)
+      new(pattern).generate_table_one
     end
 
     def initialize(pattern)
@@ -12,21 +12,28 @@ module StringSearching
       @shift = Hash.new(0)
     end
 
-    def generate
-      i = length
-      j = length + 1
-      border[i] = j
-      while i > 0
-        while j <= length && pattern[i - 1] != pattern[j - 1]
-          shift[j] = j - i if shift[j] == 0
-          j = border[j]
-        end
-        i = i - 1
-        j = j - 1
-        shift[i] = 0
-        border[i] = j
+    def generate_table_one
+      index = length
+      border_occurence = length + 1
+      border[index] = border_occurence
+      while index > 0
+        border_occurence = reset_border_if_mismatch!(border_occurence, index) - 1
+        index -= 1
+        shift[index] = 0
+        border[index] = border_occurence
       end
       shift
+    end
+
+    private
+
+    def reset_border_if_mismatch!(border_occurence, index) # rubocop:disable Metrics/AbcSize
+      while border_occurence <= length && pattern[index - 1] != pattern[border_occurence - 1]
+        shift[border_occurence] = border_occurence - index if shift[border_occurence].zero?
+        border_occurence = border[border_occurence]
+      end
+
+      border_occurence
     end
   end
 end
--- spec/string_searching/good_suffix_table_spec.rb
+++ spec/string_searching/good_suffix_table_spec.rb
@@ -1,21 +1,23 @@
 require 'spec_helper'
 
 RSpec.describe StringSearching::GoodSuffixTable do
-  it "generates shift for PICNIC" do
-    pattern = "PICNIC"
-    subject = described_class.generate(pattern)
-    expect(subject).to eq(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 3, 5 => 0, 6 => 1)
-  end
+  context 'shift table for reoccurance of suffix in pattern' do
+    it "generates shift for PICNIC" do
+      pattern = "PICNIC"
+      subject = described_class.generate_table_one(pattern)
+      expect(subject).to eq(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 3, 5 => 0, 6 => 1)
+    end
 
-  it "generates shift table for WORD" do
-    pattern = "WORD"
-    subject = described_class.generate(pattern)
-    expect(subject).to eq(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 1)
-  end
+    it "generates shift table for WORD" do
+      pattern = "WORD"
+      subject = described_class.generate_table_one(pattern)
+      expect(subject).to eq(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 1)
+    end
 
-  it "generates shift for ABBABAB" do
-    pattern = "ABBABAB"
-    subject = described_class.generate(pattern)
-    expect(subject).to eq(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 2, 5 => 0, 6 => 4, 7 => 1)
+    it "generates shift for ABBABAB" do
+      pattern = "ABBABAB"
+      subject = described_class.generate_table_one(pattern)
+      expect(subject).to eq(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 2, 5 => 0, 6 => 4, 7 => 1)
+    end
   end
 end
