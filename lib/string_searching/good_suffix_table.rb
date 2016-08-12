3bd404564b370f88965309f5abfeb08e886a5eb6
cwrw <abeer.salameh@gmail.com>
2016-08-12 13:20:38 +0300

    Add shift to good suffix table
    The shift is calculated according to the borders that cannot be extended to the
    left. This happens three times in the abbabab, twice at index 6, where
    there is a mismatch but we default to the first value, and the second
    time at index 4. The rest of the values default to zero, please note
    this is one of the good suffix shift tables that satisfies the matching
    suffix occuring somewhere else in the pattern

--- lib/string_searching/good_suffix_table.rb
+++ lib/string_searching/good_suffix_table.rb
@@ -1,6 +1,6 @@
 module StringSearching
   class GoodSuffixTable
-    attr_reader :pattern, :length
+    attr_reader :pattern, :length, :border, :shift
     def self.generate(pattern)
       new(pattern).generate
     end
@@ -8,22 +8,25 @@ module StringSearching
     def initialize(pattern)
       @pattern = pattern
       @length = pattern.length
+      @border = {}
+      @shift = Hash.new(0)
     end
 
     def generate
       i = length
       j = length + 1
-      border = {}
       border[i] = j
       while i > 0
         while j <= length && pattern[i - 1] != pattern[j - 1]
+          shift[j] = j - i if shift[j] == 0
           j = border[j]
         end
         i = i - 1
         j = j - 1
+        shift[i] = 0
         border[i] = j
       end
-      border
+      shift
     end
   end
 end
--- spec/string_searching/good_suffix_table_spec.rb
+++ spec/string_searching/good_suffix_table_spec.rb
@@ -4,18 +4,18 @@ RSpec.describe StringSearching::GoodSuffixTable do
   it "generates shift for PICNIC" do
     pattern = "PICNIC"
     subject = described_class.generate(pattern)
-    expect(subject).to eq(0 => 6, 1 => 4, 2 => 5, 3 => 6, 4 => 6, 5 => 6, 6 => 7)
+    expect(subject).to eq(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 3, 5 => 0, 6 => 1)
   end
 
-   it "generates shift table for WORD" do
+  it "generates shift table for WORD" do
     pattern = "WORD"
     subject = described_class.generate(pattern)
-    expect(subject).to eq(0 => 4, 1 => 4, 2 => 4, 3 => 4, 4 => 5)
+    expect(subject).to eq(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 1)
   end
 
   it "generates shift for ABBABAB" do
     pattern = "ABBABAB"
     subject = described_class.generate(pattern)
-    expect(subject).to eq(0 => 5, 1 => 6, 2 => 4, 3 => 5, 4 => 6, 5 => 7, 6 => 7, 7 => 8)
+    expect(subject).to eq(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 2, 5 => 0, 6 => 4, 7 => 1)
   end
 end
